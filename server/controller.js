bb5b884b95a62f946427ab8eaa5492c25da3dbd5
Kevin Ung <ung.kevin78@gmail.com>
2019-07-03 18:34:08 -0600

    updated readme

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,15 @@
+//import nodemailer
 const nodemailer = require('nodemailer')
+
+//import environment variables for your email
 const { EMAIL, PASSWORD } = process.env
+
 module.exports = {
   email: async (req, res) => {
     const { name, message, email, title, image } = req.body
 
     try {
+      //invoke the createTransport function passing in your email information. 
       let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -13,12 +18,13 @@ module.exports = {
         }
       });
 
+      //invoke the sendMail function with the info in the email
       let info = await transporter.sendMail({
-        from: `'${name}' <${email}>`,
+        from: `'${name}' <${email}>`, //This will show up when you go into the email
         to: EMAIL,
-        subject: title,
-        text: message,
-        html: `<div>${message}<div>
+        subject: title, //This will show on the subject of the email
+        text: message, //for clients with plaintext support only
+        html: `<div>${message}<div> 
               <img src="cid:unique@nodemailer.com"/>`,
         attachments: [
           { //this is the attachment of the document
